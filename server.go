a267a68487799f22776e10c589e12b6f54d7ac52
blackchestnut <alexander.kalinichev@gmail.com>
2015-01-09 18:32:06 +0300

    Rest url example.

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 )
@@ -8,12 +9,26 @@ import (
 // go run server.go
 func main() {
 	m := martini.Classic()
+
+	// Setup routes
+	r := martini.NewRouter()
+	r.Get("/", GetLanding)
+	r.Get("/test/:foo", GetTest)
+	r.Get("/arbiter/:utm/redirect/:url", GetArbiter)
+	m.Action(r.Handle)
+
 	m.Use(render.Renderer())
-	m.Get("/", func() string {
-		return "Hello world!"
-	})
-	m.Get("/ya", func(r render.Render) {
-		r.Redirect("http://ya.ru", 302)
-	})
 	m.Run()
+}
+
+func GetLanding() string {
+	return "Hellow, I`m Arbiter \nTry: /arbiter/UTM_LABEL/redirect/URL \nExample: /arbiter/foo/redirect/ya.ru"
+}
+
+func GetTest(params martini.Params) string {
+	return fmt.Sprintf("Test route params: %v, params['foo']: %v", params, params["foo"])
+}
+
+func GetArbiter(r render.Render, params martini.Params) {
+	r.Redirect("http://"+params["url"], 302)
 }
